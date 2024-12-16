91f1ded4f072977a33035f420d32c109e7168313
894risper <zilper894@gmail.com>
2024-12-16 23:09:45 +0300

    first letter capitalization

---
+++ attach.py
@@ -0,0 +1,9 @@
+def capatalize_letters (input_string):
+    capatalize_letters= input_string.title()
+    return capatalize_letters
+
+input_string= input("Enter a string ")
+
+result= capatalize_letters(input_string)
+print("Output",result)
+ 
